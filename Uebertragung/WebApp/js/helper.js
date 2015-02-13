a196a2095c2b5998c4e6388fd16aba4dc5b15bed
Kilian Köppchen <kiliankoeppchen@googlemail.com>
2015-02-13 14:48:24 +0100

    Update js and as for big pictures.

--- Uebertragung/WebApp/js/helper.js
+++ Uebertragung/WebApp/js/helper.js
@@ -1,12 +1,8 @@
 var ua2text = function(ua) {
 	var s = '';
-	// only use values RGB from RGBA per Pixel
-	// e.g. 255 1 1 255 -> 255 001 001
+	// only use values RGB from RGBA per Pixel, whitespace as delimiter
 	for (var i = 0; i < ua.length; i+=4) {
-		s += ("000" + ua[i].toString()).slice(-3);
-		s += ("000" + ua[i + 1].toString()).slice(-3);
-		s += ("000" + ua[i + 2].toString()).slice(-3);
-		console.log(s);
+		s += ua[i].toString() + ' ' + ua[i + 1].toString() + ' ' + ua[i + 2].toString() + ' ';
 	}
 	return s;
 };
--- Visualisierung/RC-AS Verbindung/as/as.cpp
+++ Visualisierung/RC-AS Verbindung/as/as.cpp
@@ -12,7 +12,6 @@
 
 #include "tinyxml2.h"
 
-
 #define DEBUG 1
 #define NUMBER_OF_COLOR_CHANNELS 3
 #define NUMBER_OF_RENDERING_CLIENTS 1
@@ -30,7 +29,7 @@ TCPsocket tcpsock;
 int rc = 0;
 
 void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
-	XMLDocument document;
+	tinyxml2::XMLDocument document;
 	document.Parse(msg->get_payload().c_str());
 	
 	//TODO alles :D
@@ -50,28 +49,24 @@ void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
 	{
 		XMLElement* data = document.FirstChildElement( "package" )->FirstChildElement( "data" );
 		
-		Uint8 pixel_data[image_dimensions[0] * image_dimensions[1] * NUMBER_OF_COLOR_CHANNELS];
-		char current_pixel_value_buffer[4];
-		current_pixel_value_buffer[4] = '\0';
-		
+		char pixel_data[image_dimensions[0] * image_dimensions[1] * NUMBER_OF_COLOR_CHANNELS];
+		char * pEnd = (char *) data->GetText();
+
 		for (int i = 0; i < image_dimensions[0] * image_dimensions[1] * NUMBER_OF_COLOR_CHANNELS; ++i)
-		{
-			std::strncpy(current_pixel_value_buffer, data->GetText()+i*3, 3);
-			char * pEnd;
-			pixel_data[i] = (Uint8) strtol(current_pixel_value_buffer, &pEnd, 10);
+		{		
+			pixel_data[i] = strtol(pEnd, &pEnd, 10);
 		}
 		
 #if DEBUG
-		for (int i = 0; i < image_dimensions[0] * image_dimensions[1] * NUMBER_OF_COLOR_CHANNELS; ++i)
+		for (int i = 0; i < 25; ++i)
 		{
-			printf("%d\n", pixel_data[i]);
+			std::cout << pixel_data[i] << std::endl;
 		}
 #endif
 			
 			
 		for (int i = 0; i < NUMBER_OF_RENDERING_CLIENTS; ++i)
 		{
-			printf("hiho\n");
 			rc = SDLNet_TCP_Send (tcpsock, pixel_data, (image_dimensions[0] * image_dimensions[1] * NUMBER_OF_COLOR_CHANNELS) + 1);
 			if(rc < image_dimensions[0] * image_dimensions[1] * NUMBER_OF_COLOR_CHANNELS) 
 			{
@@ -81,15 +76,45 @@ void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
 	}
 }
 
+static int PollEvents()
+{
+	SDL_Event event;
+	
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)
+		{
+			return 1;	
+		}
+		else if (event.type == SDL_KEYDOWN)
+		{
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				return 1;
+			}
+		}
+		else if (event.type == SDL_WINDOWEVENT)
+		{
+			//printf("WinEvent!\n");
+		}
+	}
+	return 0;
+}
+
 int main(int argc, char** argv)
 {
+	char* host_ip = "localhost";
+	if(argc > 1) {
+		host_ip = argv[1];
+	}
+	
 	if (SDLNet_Init() < 0)
 	{
 		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
 		exit(EXIT_FAILURE);
 	}
 	
-	if(SDLNet_ResolveHost(&ip, "localhost", 2000)==-1) {
+	if(SDLNet_ResolveHost(&ip, host_ip, 2000)==-1) {
 		printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
 		exit(1);
 	}
@@ -99,10 +124,13 @@ int main(int argc, char** argv)
 		printf("SDLNet_TCP_Open: %s\n", SDLNet_GetError());
 		exit(2);
 	}
-
+	websocketpp::lib::error_code error;
 	receive_server.set_message_handler(&on_message);
-	receive_server.init_asio();
-	receive_server.listen(9002);
+	receive_server.init_asio(error);
+	std::cout << error << std::endl;
+	receive_server.listen(9002, error);
+	std::cout << error << std::endl;
 	receive_server.start_accept();
 	receive_server.run();
+	//receive_server.stop_listening();
 }
