82786a95e25c801884f696253151f6c2721719af
Kilian Köppchen <kiliankoeppchen@googlemail.com>
2015-02-16 20:30:09 +0100

    Makes it compatible with the WebAPP. Note:
    custom sub-protocol is 'callback_save_data'

--- Websocket_Test/libwebsockets/test_server.cpp
+++ Websocket_Test/libwebsockets/test_server.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libwebsockets.h>
-//#include <cstring>
 
-//#pragma comment(lib, "websockets.lib")
+#ifdef __linux__
+	#include <cstring>
+#endif
 
 static int 
 callback_http(struct libwebsocket_context * that,
@@ -14,8 +15,7 @@ callback_http(struct libwebsocket_context * that,
 	return 0;
 }
 
-static int 
-callback_dumb_increment(struct libwebsocket_context * that,
+static int callback_save_data(struct libwebsocket_context * that,
 	struct libwebsocket *wsi,
 	enum libwebsocket_callback_reasons reason,
 	void *user, void *in, size_t len)
@@ -24,36 +24,8 @@ callback_dumb_increment(struct libwebsocket_context * that,
 	case LWS_CALLBACK_ESTABLISHED: // just log message that someone is connecting
 		printf("connection established\n");
 		break;
-	case LWS_CALLBACK_RECEIVE: { // the funny part
-		// create a buffer to hold our response
-		// it has to have some pre and post padding. You don't need to care
-		// what comes there, libwebsockets will do everything for you. For more info see
-		// http://git.warmcat.com/cgi-bin/cgit/libwebsockets/tree/lib/libwebsockets.h#n597
-		unsigned char *buf = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING + len +
-			LWS_SEND_BUFFER_POST_PADDING);
-
-		// pointer to `void *in` holds the incomming request
-		// we're just going to put it in reverse order and put it in `buf` with
-		// correct offset. `len` holds length of the request.
-		for (size_t i=0; i < len; i++) {
-			buf[LWS_SEND_BUFFER_PRE_PADDING + (len - 1) - i ] = ((char *) in)[i];
-		}
-
-		// log what we recieved and what we're going to send as a response.
-		// that disco syntax `%.*s` is used to print just a part of our buffer
-		// http://stackoverflow.com/questions/5189071/print-part-of-char-array
-		printf("received data: %s, replying: %.*s\n", (char *) in, (int) len,
-			buf + LWS_SEND_BUFFER_PRE_PADDING);
-
-		// send response
-		// just notice that we have to tell where exactly our response starts. That's
-		// why there's `buf[LWS_SEND_BUFFER_PRE_PADDING]` and how long it is.
-		// we know that our response has the same length as request because
-		// it's the same message in reverse order.
-		libwebsocket_write(wsi, &buf[LWS_SEND_BUFFER_PRE_PADDING], len, LWS_WRITE_TEXT);
-
-		// release memory back into the wild
-		free(buf);
+	case LWS_CALLBACK_RECEIVE: { 
+		printf("received data:%zu: %s\n Mach was damit!", len, (char *) in);
 		break;
 	}
 	default:
@@ -71,8 +43,8 @@ static struct libwebsocket_protocols protocols[] = {
 		0              // per_session_data_size
 	},
 	{
-		"dumb-increment-protocol", // protocol name - very important!
-		callback_dumb_increment,   // callback
+		"callback_save_data", // protocol name - very important!
+		callback_save_data,   // callback
 		0                          // we don't use any per session data
 	},
 	{
